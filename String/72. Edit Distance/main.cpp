1ded695ae037c9b8b0d52a995f3f61f811d62110
Minecode <minecoder@163.com>
2019-07-28 13:43:49 +0800

    String: Optimize Solution [72. Edit Distance]
    Change-Id: I89a8630e01023dd364c530e40dc90a07f0a96509

--- String/72. Edit Distance/main.cpp
+++ String/72. Edit Distance/main.cpp
@@ -27,6 +27,7 @@ using namespace std;
 
 /// Solution:
 //
+/* Solution 1:  有可优化点，见Solution2
 class Solution {
 public:
     int minDistance(string word1, string word2) {
@@ -50,17 +51,42 @@ public:
         return dp[la][lb];
     }
 };
+ */
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int la = word1.length(), lb = word2.length();
+        // dp[i][j] means the step convert word1[0...i-1) to word2[0...j-1)
+        vector<int> pre (lb+1, 0);
+        vector<int> cur (lb+1, 0);
+        for (int i = 1; i <= lb; ++i)
+            pre[i] = i;
+
+        for (int i = 1; i <= la; ++i) {
+            cur[0] = i;
+            for (int j = 1; j <= lb; ++j) {
+                if (word1[i-1] == word2[j-1])
+                    cur[j] = pre[j-1];
+                else
+                    cur[j] = min(pre[j-1], min(cur[j-1], pre[j])) + 1;
+            }
+            fill(pre.begin(), pre.end(), 0);
+            swap(pre, cur);
+        }
+        return pre[lb];
+    }
+};
 
 int main() {
     Solution sol = Solution();
-//    string word1 = "horse";
-//    string word2 = "ros";
+    string word1 = "horse";
+    string word2 = "ros";
 //    string word1 = "intention";
 //    string word2 = "execution";
 //    string word1 = "horse";
 //    string word2 = "";
-    string word1 = "";
-    string word2 = "ros";
+//    string word1 = "";
+//    string word2 = "ros";
     int res = sol.minDistance(word1, word2);
     cout << res << endl;
     return 0;
