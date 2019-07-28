b00f6ce5a207ffae536e74c504cbe8a9d155c842
Minecode <minecoder@163.com>
2019-07-28 13:32:00 +0800

    String: Add Solution [72. Edit Distance]
    Change-Id: Idab761eaa59fa1ee53a965d32f0cdee3f1b7e036

---
+++ String/72. Edit Distance/main.cpp
@@ -0,0 +1,67 @@
+//
+//  main.cpp
+//  72. Edit Distance
+//
+//  Created by 边俊林 on 2019/7/28.
+//  Copyright © 2019 Minecode.Link. All rights reserved.
+//
+
+/* ------------------------------------------------------ *\
+ https://leetcode.com/problems/edit-distance/
+\* ------------------------------------------------------ */
+
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+/// Solution:
+//
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int la = word1.length(), lb = word2.length();
+        // dp[i][j] means the step convert word1[0...i-1) to word2[0...j-1)
+        vector<vector<int>> dp (la+1, vector<int> (lb+1, 0));
+        for (int i = 1; i <= la; ++i)
+            dp[i][0] = i;
+        for (int i = 1; i <= lb; ++i)
+            dp[0][i] = i;
+
+        for (int i = 1; i <= la; ++i) {
+            for (int j = 1; j <= lb; ++j) {
+                if (word1[i-1] == word2[j-1])
+                    dp[i][j] = dp[i-1][j-1];
+                else
+                    dp[i][j] = min(dp[i-1][j-1], min(dp[i][j-1], dp[i-1][j])) + 1;
+            }
+        }
+
+        return dp[la][lb];
+    }
+};
+
+int main() {
+    Solution sol = Solution();
+//    string word1 = "horse";
+//    string word2 = "ros";
+//    string word1 = "intention";
+//    string word2 = "execution";
+//    string word1 = "horse";
+//    string word2 = "";
+    string word1 = "";
+    string word2 = "ros";
+    int res = sol.minDistance(word1, word2);
+    cout << res << endl;
+    return 0;
+}
