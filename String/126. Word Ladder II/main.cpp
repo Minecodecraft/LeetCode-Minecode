5582fa696ca7c64ad6657c7392f9bf4b89472d6c
Minecode <minecoder@163.com>
2019-08-05 00:46:12 +0800

    String: Add Solution [126. Word Ladder II]
    Change-Id: I54254e1326a0e0de8e0cefcc5a4b7cbf7016ff7a

--- String/126. Word Ladder II/main.cpp
+++ String/126. Word Ladder II/main.cpp
@@ -31,45 +31,68 @@ using namespace std;
 class Solution {
 public:
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        vector<vector<string>> res;
+        if (beginWord == endWord)
+            return {{beginWord}};
+        vector<vector<string>> paths;
         vector<string> path = {beginWord};
-        vector<bool> vis (wordList.size(), false);
-        int minn = INT_MAX;
-        findCore(beginWord, endWord, wordList, path, vis, res, 0, minn);
-        vector<vector<string>> rres;
-        for (int i = 0; i < res.size(); ++i) {
-            if (res[i].size() == minn)
-                rres.push_back(res[i]);
-        }
-        return rres;
+        unordered_set<string> wl (wordList.begin(), wordList.end());
+        unordered_set<string> q1, q2;
+        unordered_map<string, vector<string>> mp;
+        if (wl.find(endWord) == wl.end())
+            return {};
+        q1.insert(beginWord);
+        q2.insert(endWord);
+        wl.erase(beginWord);
+        wl.erase(endWord);
+        if (findPath(q1, q2, wl, mp, 0))
+            getPath(beginWord, endWord, path, paths, mp);
+        return paths;
     }
 
 private:
-    void findCore(string beg, string end, vector<string>& wl, vector<string>& path, vector<bool> vis, vector<vector<string>>& res, int depth, int& minn) {
-        if (beg == end) {
-            res.push_back(path);
-            minn = min(minn, (int)path.size());
-            return;
-        }
-        if (depth >= wl.size() || depth > minn) return;
+    // 0 frontaled 1 reversed
+    bool findPath(unordered_set<string>& q1, unordered_set<string>& q2, unordered_set<string> wl, unordered_map<string, vector<string>>& mp, int dir) {
+        if (q1.empty()) return false;
+        if (q1.size() > q2.size())
+            return findPath(q2, q1, wl, mp, 1-dir);
 
-        for (int i = 0; i < wl.size(); ++i) {
-            if (canPickup(beg, wl[i]) && !vis[i]) {
-                vis[i] = true;
-                path.push_back(wl[i]);
-                findCore(wl[i], end, wl, path, vis, res, depth+1, minn);
-                path.pop_back();
-                vis[i] = false;
+        bool match = false;
+        unordered_set<string> tmp;
+        vector<string> removelist;
+        for (auto it = q1.begin(); it != q1.end(); ++it) {
+            string word = *it;
+            for (int i = 0; i < word.length(); ++i) {
+                char ch = word[i];
+                for (char j = 'a'; j <= 'z'; ++j) {
+                    if (ch == j) continue;
+                    word[i] = j;
+                    if (q2.find(word) != q2.end()) {
+                        match = true;
+                        dir == 0 ? mp[*it].push_back(word) : mp[word].push_back(*it);
+                    } else if (!match && wl.find(word) != wl.end()) {
+                        tmp.insert(word);
+                        removelist.push_back(word);
+                        dir == 0 ? mp[*it].push_back(word) : mp[word].push_back(*it);
+                    }
+                }
+                word[i] = ch;
             }
         }
+        for (auto word: removelist)
+            wl.erase(word);
+        return match || findPath(tmp, q2, wl, mp, dir);
     }
 
-    bool canPickup(string ori, string tar) {
-        int cnt = 0;
-        for (int i = 0; i < tar.length(); ++i) {
-            if (ori[i] != tar[i]) cnt++;
+    void getPath(string beg, string end, vector<string>& path, vector<vector<string>>& paths, unordered_map<string, vector<string>>& mp) {
+        if (beg == end) {
+            paths.push_back(path);
+        } else {
+            for (auto word: mp[beg]) {
+                path.push_back(word);
+                getPath(word, end, path, paths, mp);
+                path.pop_back();
+            }
         }
-        return cnt == 1;
     }
 };
 
@@ -91,12 +114,15 @@ void printVectors(vector<vector<string>> v) {
 
 int main() {
     Solution sol = Solution();
+    string b = "hit";
+    string e = "cog";
+    vector<string> wl = {"hot","dot","dog","lot","log"};
 //    string b = "hit";
 //    string e = "cog";
 //    vector<string> wl = {"hot","dot","dog","lot","log","cog"};
-    string b = "qa";
-    string e = "sq";
-    vector<string> wl = {"si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"};
+//    string b = "qa";
+//    string e = "sq";
+//    vector<string> wl = {"si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"};
     auto res = sol.findLadders(b, e, wl);
     printVectors(res);
     return 0;
