de44645414661c2ac65e6ffec342efa49c69142c
Minecode <minecoder@163.com>
2019-09-24 20:54:43 +0800

    Sliding Window: Add Optimization [995. Minimum Number of K Consecutive Bit Flips]
    Change-Id: I4698a220ed9968539ebe44a649bf325f6aa9d6e0

--- Sliding Window/995. Minimum Number of K Consecutive Bit Flips/main.cpp
+++ Sliding Window/995. Minimum Number of K Consecutive Bit Flips/main.cpp
@@ -27,19 +27,45 @@ using namespace std;
 
 /// Solution:
 //
+// Solution 1: Normal Solution
+// O(n) Time Complexity O(n) Space Complexity
+/*
 class Solution {
 public:
     int minKBitFlips(vector<int>& A, int K) {
         queue<int> Q;
         int res = 0;
         for (int i = 0; i < A.size(); ++i) {
-            if (A[i] != 1-(Q.size() % 2)) {
+            if (A[i] == Q.size() % 2) {
                 if (i + K - 1 >= A.size())
                     return -1;
                 Q.push(i + K - 1);
                 ++res;
             }
             while (!Q.empty() && Q.front() <= i) Q.pop();
+        }
+        return res;
+    }
+};
+ */
+
+// Solution 2: Optimize memory usage
+// O(n) Time Complexity O(1) Space Complexity
+class Solution {
+public:
+    int minKBitFlips(vector<int>& A, int K) {
+        int res = 0, flips = 0;
+        for (int i = 0; i < A.size(); ++i) {
+            if (A[i] == (flips & 1)) {
+                if (i + K - 1 >= A.size()) return -1;
+                A[i] -= 2;
+                ++flips;
+                ++res;
+            }
+            while (i-K+1 >= 0 && A[i - K + 1] < 0) {
+                --flips;
+                A[i - K + 1] += 2;
+            }
         }
         return res;
     }
