3a26514db3e84c028aa5e4f322bb17d3c717f31d
Minecode <minecoder@163.com>
2019-10-10 12:56:04 +0800

    Stack & Heap: Add Solution [739. Daily Temperatures]
    Change-Id: I88beae2650ffbda6923d1aec09eacce30104b634

---
+++ Stack & Heap/739. Daily Temperatures/main.cpp
@@ -0,0 +1,61 @@
+//
+//  main.cpp
+//  739. Daily Temperatures
+//
+//  Created by 边俊林 on 2019/10/10.
+//  Copyright © 2019 Minecode.Link. All rights reserved.
+//
+
+/* ------------------------------------------------------ *\
+ https://leetcode.com/problems/daily-temperatures/
+\* ------------------------------------------------------ */
+
+#include <map>
+#include <set>
+#include <queue>
+#include <string>
+#include <stack>
+#include <vector>
+#include <cstdio>
+#include <numeric>
+#include <cstdlib>
+#include <utility>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+/// Solution:
+//
+class Solution {
+public:
+    using pir = pair<int, int>;
+    vector<int> dailyTemperatures(vector<int>& T) {
+        auto cmp = [](pir& a, pir& b) {
+            return a.first > b.first;
+        };
+        vector<int> res (T.size(), 0);
+        priority_queue<pir, vector<pir>, decltype(cmp)> pq (cmp);
+        for (int i = 0; i < T.size(); ++i) {
+            int cur = T[i];
+            while (pq.size() && pq.top().first < cur) {
+                pir pre = pq.top(); pq.pop();
+                res[pre.second] = i - pre.second;
+            }
+            pq.push({cur, i});
+        }
+        return res;
+    }
+};
+
+int main() {
+    Solution sol = Solution();
+    vector<int> arr = {
+        73, 74, 75, 71, 69, 72, 76, 73
+    };
+    auto res = sol.dailyTemperatures(arr);
+    for_each(res.begin(), res.end(), [](int& elem) { cout << elem << ",";});
+    return 0;
+}
