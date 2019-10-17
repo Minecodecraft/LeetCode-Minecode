451a62554944a60a56c7e5da7844db5c3efb754a
Minecode <minecoder@163.com>
2019-10-17 11:19:59 +0800

    T100L: Add Solution [226. Invert Binary Tree]
    Change-Id: I18483f7fc95b0182d50ee63fd73702aad236d328

---
+++ Top 100 Liked/226. Invert Binary Tree/main.cpp
@@ -0,0 +1,49 @@
+//
+//  main.cpp
+//  226. Invert Binary Tree
+//
+//  Created by 边俊林 on 2019/10/17.
+//  Copyright © 2019 Minecode.Link. All rights reserved.
+//
+
+#include <map>
+#include <set>
+#include <queue>
+#include <string>
+#include <stack>
+#include <vector>
+#include <cstdio>
+#include <numeric>
+#include <cstdlib>
+#include <utility>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+/// Solution:
+//
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == NULL) return NULL;
+        root->right = invertTree(root->right);
+        root->left = invertTree(root->left);
+        swap(root->left, root->right);
+        return root;
+    }
+};
+
+int main() {
+
+    return 0;
+}
