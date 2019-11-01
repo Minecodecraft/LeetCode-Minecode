24d7be06d82cde86e7f5f36ab822589d4287e144
Minecode <minecoder@163.com>
2019-11-01 11:38:02 +0800

    Greedy: Init Proj
    Change-Id: I36a7327e8c69e345ec3138db51892acbb6ed7d24

---
+++ Greedy/Template/main.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  Template
+//
+//  Created by 边俊林 on 2019/11/1.
+//  Copyright © 2019 边俊林. All rights reserved.
+//
+
+#include <map>
+#include <set>
+#include <queue>
+#include <string>
+#include <stack>
+#include <vector>
+#include <cstdio>
+#include <numeric>
+#include <cstdlib>
+#include <utility>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+/// Solution:
+//
+
+
+int main() {
+
+    return 0;
+}
