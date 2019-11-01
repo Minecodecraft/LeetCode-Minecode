2fd7a5b415e790057a7047985539efe9e0aad7a2
Minecode <minecoder@163.com>
2019-11-01 11:07:18 +0800

    TIQ: Add Optimization [395. Longest Substring with At Least K Repeating Characters]
    Change-Id: Ied8709c813f1c798d89be28a68711241ee66df44

--- Top Interview Questions/395. Longest Substring with At Least K Repeating Characters/main.cpp
+++ Top Interview Questions/395. Longest Substring with At Least K Repeating Characters/main.cpp
@@ -25,6 +25,8 @@ using namespace std;
 
 /// Solution:
 //
+// Solution 1: O(n) avg and O(n^2) in worst case, a improvement of brute force
+/*
 class Solution {
 public:
     int longestSubstring(string s, int k) {
@@ -52,15 +54,44 @@ public:
         return res;
     }
 };
+ */
+
+// Solution 2: O(n) in avg and worst case, 26 level recursions at most
+class Solution {
+public:
+    int longestSubstring(string s, int k) {
+        return getLong(s, k, 0, s.length());
+    }
+    
+private:
+    int getLong(const string& s, int k, int l, int r) {
+        vector<int> cnt (26, 0);
+        for (int i = l; i < r; ++i)
+            cnt[s[i]-'a']++;
+        
+        int res = 0;
+        for (; l < r; ) {
+            int ll = l;
+            while (ll < r && cnt[s[ll]-'a'] < k) ++ll;
+            if (ll == r) break;
+            int rr = ll;
+            while (rr < r && cnt[s[rr]-'a'] >= k) ++rr;
+            if (ll == l && rr == r) return r-l;
+            res = max(res, getLong(s, k, ll, rr));
+            l = rr;
+        }
+        return res;
+    }
+};
 
 int main() {
     Solution sol = Solution();
 //    string s = "aaabb";
-//    string s = "ababbc";
-    string s = "";
+    string s = "ababbc";
+//    string s = "";
 //    int k = 3;
-//    int k = 2;
-    int k = 1;
+    int k = 2;
+//    int k = 1;
     int res = sol.longestSubstring(s, k);
     cout << res << endl;
     return 0;
