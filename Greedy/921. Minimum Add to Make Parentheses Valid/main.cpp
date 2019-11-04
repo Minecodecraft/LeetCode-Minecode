f7c70059019404c49b2552eeda459b5a2ca229cc
Minecode <minecoder@163.com>
2019-11-04 14:09:38 +0800

    Greedy: Add Solution [921. Minimum Add to Make Parentheses Valid]
    Change-Id: Ibf337b6a638150c0d5e87139a93007541a698551

---
+++ Greedy/921. Minimum Add to Make Parentheses Valid/main.cpp
@@ -0,0 +1,38 @@
+//
+//  main.cpp
+//  921. Minimum Add to Make Parentheses Valid
+//
+//  Created by 边俊林 on 2019/11/4.
+//  Copyright © 2019 边俊林. All rights reserved.
+//
+
+#include <map>
+#include <set>
+#include <queue>
+#include <string>
+#include <stack>
+#include <vector>
+#include <cstdio>
+#include <numeric>
+#include <cstdlib>
+#include <utility>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+/// Solution:
+//
+class Solution {
+public:
+    int minAddToMakeValid(string S) {
+        
+    }
+};
+
+int main() {
+
+    return 0;
+}
