384ee4c42f679732b6daea1f564430309b8f026a
Minecode <minecoder@163.com>
2019-11-07 22:14:03 +0800

    Greedy: Add Optimization [1029. Two City Scheduling]
    Change-Id: I4e920d0d086c6d7d71f285057ba80483e2efdfdc

--- Greedy/1029. Two City Scheduling/main.cpp
+++ Greedy/1029. Two City Scheduling/main.cpp
@@ -42,15 +42,41 @@ public:
 };
  */
 
-// Solution 2: O(n)
+// Solution 2: O(n) average with QuickSelection algorithm, beats 100%
 class Solution {
 public:
     int twoCitySchedCost(vector<vector<int>>& costs) {
+        quickSelect(costs, 0, costs.size()-1, costs.size()/2);
+        int res = 0;
+        for (int i = 0; i < costs.size()/2; ++i)
+            res += costs[i][0] + costs[i+costs.size()/2][1];
+        return res;
+    }
 
+private:
+    void quickSelect(vector<vector<int>>& costs, int low, int high, int k) {
+        int pivot = low + rand() % (high-low+1);
+        swap(costs[high], costs[pivot]);
+        int i = low, j = low;
+        while (j < high) {
+            if (costs[j][0]-costs[j][1] <= costs[high][0]-costs[high][1])
+                swap(costs[i++], costs[j]);
+            ++j;
+        }
+        swap(costs[i], costs[j]);
+        if (i == high-k+1) return;
+        else if (i > high-k+1) quickSelect(costs, low, i-1, k-(high-i+1));
+        else quickSelect(costs, i+1, high, k);
     }
 };
 
 int main() {
+    Solution sol = Solution();
 
+    vector<vector<int>> mat = {
+        {10, 20}, {30, 200}, {400, 50}, {30, 20}
+    };
+    int res = sol.twoCitySchedCost(mat);
+    cout << res << endl;
     return 0;
 }
