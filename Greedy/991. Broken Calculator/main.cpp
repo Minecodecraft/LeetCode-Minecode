caa695d61a1c3af6b8ddb85afbca8a5b9edb3db4
Minecode <minecoder@163.com>
2019-11-07 19:57:20 +0800

    Greedy: Add Solution [991. Broken Calculator]
    Change-Id: I91383c7a27a782c43b035899e4c1d0875aedd2da

---
+++ Greedy/991. Broken Calculator/main.cpp
@@ -0,0 +1,52 @@
+//
+//  main.cpp
+//  991. Broken Calculator
+//
+//  Created by 边俊林 on 2019/11/7.
+//  Copyright © 2019 边俊林. All rights reserved.
+//
+
+#include <map>
+#include <set>
+#include <queue>
+#include <string>
+#include <stack>
+#include <vector>
+#include <cstdio>
+#include <numeric>
+#include <cstdlib>
+#include <utility>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+/// Solution:
+//
+class Solution {
+public:
+    int brokenCalc(int x, int y) {
+        int res = 0;
+
+        while (y > x) {
+            if (y & 1) {
+                y++;
+                res++;
+            }
+            y >>= 1;
+            res++;
+        }
+        res += x-y;
+        return res;
+    }
+};
+
+int main() {
+    Solution sol = Solution();
+    int x= 3, y = 10;
+    int res = sol.brokenCalc(x, y);
+    cout << res << endl;
+    return 0;
+}
