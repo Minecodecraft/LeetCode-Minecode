7c364795a2a60a0138ef9ecd052b1f4f29f71e1b
Minecode <minecoder@163.com>
2019-11-14 11:46:12 +0800

    Graph: Add Solution [310. Minimum Height Trees]
    Change-Id: If817f689aa09de0ef2dccf9334ae264dffa20d0e

---
+++ Graph/310. Minimum Height Trees/main.cpp
@@ -0,0 +1,102 @@
+//
+//  main.cpp
+//  310. Minimum Height Trees
+//
+//  Created by 边俊林 on 2019/11/13.
+//  Copyright © 2019 边俊林. All rights reserved.
+//
+
+#include <map>
+#include <set>
+#include <queue>
+#include <string>
+#include <stack>
+#include <vector>
+#include <cstdio>
+#include <numeric>
+#include <cstdlib>
+#include <utility>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+/// Solution:
+//
+class Solution {
+public:
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        if (n == 1) return {0};
+
+        vector<int> degree (n, 0);
+        unordered_map<int, vector<int>> mp;
+        for (auto& e: edges) {
+            degree[e[0]]++;
+            degree[e[1]]++;
+            mp[e[0]].push_back(e[1]);
+            mp[e[1]].push_back(e[0]);
+        }
+        int last = n;
+        queue<int> q;
+
+        for (int i = 0; i < n; ++i)
+            if (degree[i] == 1) q.push(i);
+
+        while (q.size() && last > 2) {
+            int p = q.front(); q.pop();
+            degree[p]--;
+            last--;
+            for (auto it = mp[p].begin(); it != mp[p].end(); ++it) {
+                if (--degree[*it] == 1)
+                    q.push(*it);
+            }
+        }
+
+        map<int, vector<int>> cnt;
+        while (q.size()) {
+            int p = q.front(); q.pop();
+            int num = countstep(mp, p, n);
+            cnt[num].push_back(p);
+        }
+        return cnt.empty() ? vector<int>() : cnt.begin()->second;
+    }
+
+private:
+    const long long SEED = 1e6;
+    int countstep(unordered_map<int, vector<int>>& mp, int idx, int n) {
+        queue<long long> q;
+        vector<bool> vis (n, false);
+        int maxlevel = 0;
+        q.push(idx + 1 * SEED);
+        while (q.size()) {
+            long cp = q.front(); q.pop();
+            int level = cp / SEED;
+            int p = cp % SEED;
+            if (vis[p]) continue;
+            vis[p] = true;
+            maxlevel = max(maxlevel, level);
+
+            for (auto it = mp[p].begin(); it != mp[p].end(); ++it) {
+                q.push(*it + (level+1) * SEED);
+            }
+        }
+        return maxlevel;
+    }
+};
+
+int main() {
+    Solution sol = Solution();
+//    int n = 4;
+//    int n = 6;
+    int n = 1;
+    vector<vector<int>> mat = {
+//        {1, 0}, {1, 2}, {1, 3}
+//        [0, 3], [1, 3], [2, 3], [4, 3], [5, 4]
+    };
+    auto res = sol.findMinHeightTrees(n, mat);
+    for_each(begin(res), end(res), [](int ele) { cout << ele << ", "; });
+    cout << endl;
+    return 0;
+}
