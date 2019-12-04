bb953d5f02dc54540b57d282a936d3d6012bd22f
Minecode <minecoder@163.com>
2019-12-04 09:02:05 +0800

    Graph: Add Solution [1042. Flower Planting With No Adjacent]
    Change-Id: I18b1cfc01af934458cc786d4720c0bd9a3aa7de6

---
+++ Graph/1042. Flower Planting With No Adjacent/main.cpp
@@ -0,0 +1,38 @@
+//
+//  main.cpp
+//  1042. Flower Planting With No Adjacent
+//
+//  Created by 边俊林 on 2019/12/4.
+//  Copyright © 2019 边俊林. All rights reserved.
+//
+
+#include <map>
+#include <set>
+#include <queue>
+#include <string>
+#include <stack>
+#include <vector>
+#include <cstdio>
+#include <numeric>
+#include <cstdlib>
+#include <utility>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+/// Solution:
+//
+class Solution {
+public:
+    vector<int> gardenNoAdj(int N, vector<vector<int>>& paths) {
+
+    }
+};
+
+int main() {
+
+    return 0;
+}
