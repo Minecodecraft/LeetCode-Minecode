65280410e9742f652ff0380c890535d3efed944c
Minecode <minecoder@163.com>
2019-12-27 21:15:06 +0800

    Google: Add Optimization [308. Range Sum Query 2D - Mutable]
    Change-Id: Id973ad0b18603120d8dbbaa88921dcb875df13d2

--- Company-Google/308. Range Sum Query 2D - Mutable/main.cpp
+++ Company-Google/308. Range Sum Query 2D - Mutable/main.cpp
@@ -59,7 +59,43 @@ private:
 };
  */
 
+// Solution 2: 2D-BIT(Binary indexed tree), 16ms, 14.3M
+class NumMatrix {
+public:
+    NumMatrix(vector<vector<int>>& matrix) {
+        n = matrix.size(), m = n == 0 ? 0 : matrix[0].size();
+        bit = vector<vector<int>> (n+1, vector<int> (m+1, 0));
+        nums = vector<vector<int>> (n, vector<int> (m, 0));
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < m; ++j)
+                update(i, j, matrix[i][j]);
+    }
 
+    void update(int row, int col, int val) {
+        int dif = val - nums[row][col];
+        nums[row][col] = val;
+        for (int i = row+1; i <= n; i += (i & -i))
+            for (int j = col+1; j <= m; j += (j & -j))
+                bit[i][j] += dif;
+    }
+
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return sumRange(row2, col2) - sumRange(row1-1, col2) - sumRange(row2, col1-1) + sumRange(row1-1, col1-1);
+    }
+
+private:
+    int n, m;
+    vector<vector<int>> bit;
+    vector<vector<int>> nums;
+
+    int sumRange(int row, int col) {
+        int sum = 0;
+        for (int i = row+1; i > 0; i -= (i & -i))
+            for (int j = col+1; j > 0; j -= (j & -j))
+                sum += bit[i][j];
+        return sum;
+    }
+};
 
 int main() {
     vector<vector<int>> mat = {
