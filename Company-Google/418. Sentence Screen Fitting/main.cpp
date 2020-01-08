a03baaeb3898c24d377781e143beee9b16c77a04
Minecode <minecoder@163.com>
2020-01-08 10:46:55 +0800

    Google: Add Solution [418. Sentence Screen Fitting]
    Change-Id: Ib54bccb914e5f9eca99e378d0b3b03c680403abc

---
+++ Company-Google/418. Sentence Screen Fitting/main.cpp
@@ -0,0 +1,38 @@
+//
+//  main.cpp
+//  418. Sentence Screen Fitting
+//
+//  Created by 边俊林 on 2020/1/7.
+//  Copyright © 2020 边俊林. All rights reserved.
+//
+
+#include <map>
+#include <set>
+#include <queue>
+#include <string>
+#include <stack>
+#include <vector>
+#include <cstdio>
+#include <numeric>
+#include <cstdlib>
+#include <utility>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+/// Solution:
+//
+class Solution {
+public:
+    int wordsTyping(vector<string>& sentence, int rows, int cols) {
+
+    }
+};
+
+int main() {
+
+    return 0;
+}
