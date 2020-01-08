a5a2f1c86e63579dbf753652b98353d195b07f3e
Minecode <minecoder@163.com>
2020-01-08 17:06:12 +0800

    Google: Add Optimization [418. Sentence Screen Fitting]
    Change-Id: I29dea7f4cf70912750e8d63b346ef9ad67164482

--- Company-Google/418. Sentence Screen Fitting/main.cpp
+++ Company-Google/418. Sentence Screen Fitting/main.cpp
@@ -25,12 +25,57 @@ using namespace std;
 
 /// Solution:
 //
+// Solution 1: Brute Force, 4ms, beats 90%
+/*
 class Solution {
 public:
     int wordsTyping(vector<string>& sentence, int rows, int cols) {
-
+        string pattern = "";
+        for (auto& str: sentence)
+            pattern += str, pattern += ' ';
+        int p = 0, len = pattern.length();
+        for (int i = 0; i < rows; ++i) {
+            p += cols;
+            if (pattern[p % len] == ' ') {
+                ++p;
+            } else {
+                while (p > 0 && pattern[p % len] != ' ') {
+                    --p;
+                }
+                ++p;
+            }
+        }
+        return p / len;
     }
 };
+ */
+
+// Solution 2: Memo solution, 28ms beats 30%
+/*
+class Solution {
+public:
+    int wordsTyping(vector<string>& sentence, int rows, int cols) {
+        unordered_map<int, int> cnt;
+        int p = 0, n = sentence.size();
+        for (int i = 0; i < rows; ++i) {
+            if (cnt.count(p % n) > 0) {
+                p += cnt[p % n];
+            } else {
+                int begp = p, cp = 0, tmp = 0;
+                while (cp + sentence[p % n].length() <= cols) {
+                    cp += sentence[p % n].length() + 1;
+                    ++p;
+                    ++tmp;
+                }
+                p = begp + (cnt[begp % n] = tmp);
+            }
+        }
+        return p / n;
+    }
+};
+ */
+
+
 
 int main() {
 
