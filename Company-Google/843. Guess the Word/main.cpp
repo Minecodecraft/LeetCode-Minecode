0d2da043abb60b55270b784378fa5de082611b78
Minecoder <minecoder@163.com>
2020-04-15 21:09:04 +0800

    [Google] Add Optimization [843. Guess the Word]

--- Company-Google/843. Guess the Word/main.cpp
+++ Company-Google/843. Guess the Word/main.cpp
@@ -25,6 +25,8 @@ using namespace std;
 
 /// Solution:
 //
+// Solution 1: Random guess, O(n) time complexity
+/*
 class Solution {
 public:
     void findSecretWord(vector<string>& words, Master& master) {
@@ -51,6 +53,43 @@ private:
         for (int i = 0; i < a.length(); ++i) {
             if (a[i] == b[i]) ++cnt;
         }
+        return cnt;
+    }
+};
+ */
+
+// Solution 2: Minimax algorithm, O(n^2) time complexity
+class Solution {
+public:
+    void findSecretWord(vector<string>& wordlist, Master& master) {
+        for (int i = 0; i < 10; ++i) {
+            unordered_map<string, int> same;
+            for (auto& str1: wordlist) {
+                for (auto& str2: wordlist) {
+                    if (match(str1, str2) == 0)
+                        same[str1]++;
+                }
+            }
+            pair<string, int> minimax = {wordlist[0], INT_MAX};
+            for (auto& str: wordlist) {
+                if (same[str] < minimax.second)
+                    minimax = {str, same[str]};
+            }
+            int res = master.guess(minimax.first);
+            if (res == 6) return;
+            vector<string> newlist;
+            for (auto& str: wordlist)
+                if (str != minimax.first && match(minimax.first, str) == res)
+                    newlist.push_back(str);
+            wordlist = newlist;
+        }
+    }
+
+private:
+    int match(string& a, string& b) {
+        int cnt = 0;
+        for (int i = 0; i < a.length(); ++i)
+            if (a[i] == b[i]) ++cnt;
         return cnt;
     }
 };
