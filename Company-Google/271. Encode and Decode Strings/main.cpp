39cb3c2bb78d1a075e50f3f07f9768784838fea7
Minecoder <minecoder@163.com>
2020-04-15 21:31:55 +0800

    Google: Add Solution [271. Encode and Decode Strings]

---
+++ Company-Google/271. Encode and Decode Strings/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  271. Encode and Decode Strings
+//
+//  Created by Jaylen Bian on 4/15/20.
+//  Copyright © 2020 边俊林. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
