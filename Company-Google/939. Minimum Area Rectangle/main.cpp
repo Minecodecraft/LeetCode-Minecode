d94bf01ee7ca165cc1320c643688edb38dd1e59a
Minecoder <minecoder@163.com>
2020-08-07 18:39:39 +0800

    Google: Add Solution 2 [939. Minimum Area Rectangle]

--- Company-Google/939. Minimum Area Rectangle/main.cpp
+++ Company-Google/939. Minimum Area Rectangle/main.cpp
@@ -25,6 +25,8 @@ using namespace std;
 
 /// Solution:
 //
+// Solution 1: Best
+/*
 class Solution {
 
     int idx(int x, int y) {
@@ -52,6 +54,34 @@ public:
                         if (ext.count(idx(x2, y1)) && ext.count(idx(x2, y2)))
                             res = min(res, abs((x2-x1) * (y2-y1)));
                     }
+                }
+            }
+        }
+        return res == INT_MAX ? 0 : res;
+    }
+};
+ */
+
+// Solution 2: Not the best, but it's also a good way.
+class Solution {
+    int idx(int x, int y) {
+        return x * 40001 + y;
+    }
+public:
+    int minAreaRect(vector<vector<int>>& pts) {
+        unordered_set<int> ext;
+        for (auto& pt: pts)
+            ext.insert(idx(pt[0], pt[1]));
+        int res = INT_MAX;
+        for (int i = 0; i < pts.size(); ++i) {
+            for (int j = i+1; j < pts.size(); ++j) {
+                int x1 = pts[i][0], x2 = pts[j][0];
+                int y1 = pts[i][1], y2 = pts[j][1];
+                if (x1 == x2 || y1 == y2)
+                    continue;
+                if (ext.count(idx(x1, y2))
+                    && ext.count(idx(x2, y1))) {
+                    res = min(res, abs((x2-x1) * (y2-y1)));
                 }
             }
         }
