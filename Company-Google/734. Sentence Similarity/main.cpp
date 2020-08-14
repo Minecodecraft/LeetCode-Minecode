2e5d418ae004dabfa5674cbeb359445a25e4d2f4
Minecoder <minecoder@163.com>
2020-08-15 07:53:56 +0800

    Google: Add Solution [734. Sentence Similarity]

--- Company-Google/734. Sentence Similarity/main.cpp
+++ Company-Google/734. Sentence Similarity/main.cpp
@@ -27,8 +27,19 @@ using namespace std;
 //
 class Solution {
 public:
-    bool areSentencesSimilar(vector<string>& words1, vector<string>& words2, vector<vector<string>>& pairs) {
-
+    bool areSentencesSimilar(vector<string>& ws1, vector<string>& ws2, vector<vector<string>>& prs) {
+        if (ws1.size() != ws2.size())
+            return false;
+        unordered_map<string, unordered_set<string>> match;
+        for (auto& pr: prs) {
+            match[pr[0]].insert(pr[1]);
+            match[pr[1]].insert(pr[0]);
+        }
+        for (int i = 0; i < ws1.size(); ++i) {
+            if (ws1[i] != ws2[i] && !match[ws1[i]].count(ws2[i]) && !match[ws2[i]].count(ws1[i]))
+                return false;
+        }
+        return true;
     }
 };
 
