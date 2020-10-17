7dcd305e5c822f07e8845492eb30fb816c810143
Minecoder <minecoder@163.com>
2020-10-17 23:18:19 +0800

    Google: Add Optimization [305. Number of Islands II]

--- Company-Google/305. Number of Islands II/main.cpp
+++ Company-Google/305. Number of Islands II/main.cpp
@@ -25,6 +25,8 @@ using namespace std;
 
 /// Solution:
 //
+// Solution 1
+/*
 class Solution {
     vector<int> f;
     int dx[4] = {-1, 0, 1, 0};
@@ -57,6 +59,63 @@ public:
                         f[newpidx] = pidx;
                         --cnt;
                     }
+                }
+            }
+            res.push_back(cnt);
+        }
+        return res;
+    }
+};
+ */
+
+// Solution 2
+// Use rank to optimize
+class Solution {
+    vector<int> f, rank;
+    int dx[4] = {-1, 0, 1, 0};
+    int dy[4] = {0, -1, 0, 1};
+
+    int Find(int u) {
+        if (f[u] == u) return u;
+        return f[u] = Find(f[u]);
+    }
+
+    bool Union(int u, int v) {
+        int pu = Find(u), pv = Find(v);
+        if (pu == pv)
+            return false;
+
+        if (rank[pu] == rank[pv]) {
+            f[pu] = pv;
+            ++rank[pv];
+        } else if (rank[pu] > rank[pv]) {
+            f[pv] = pu;
+        } else {
+            f[pu] = pv;
+        }
+        return true;
+    }
+
+public:
+    vector<int> numIslands2(int m, int n, vector<vector<int>>& poss) {
+        f = vector<int> (m*n, -1);
+        rank = vector<int> (m*n, 0);
+
+        vector<int> res;
+        int cnt = 0;
+        for (auto& pos: poss) {
+            int x = pos[0], y = pos[1], idx = x * n + y;
+            if (f[idx] != -1) {
+                res.push_back(cnt);
+                continue;
+            }
+            f[idx] = idx;
+            ++cnt;
+            for (int i = 0; i < 4; ++i) {
+                int tx = x + dx[i], ty = y + dy[i], newidx = tx * n + ty;
+                if (tx >= 0 && tx < m && ty >= 0 && ty < n && f[newidx] != -1) {
+                    if (Union(idx, newidx))
+                        --cnt;
                 }
             }
             res.push_back(cnt);
