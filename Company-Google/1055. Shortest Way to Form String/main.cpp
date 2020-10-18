851415cd2120d1f3069281ed102337bf43a51da3
Minecoder <minecoder@163.com>
2020-10-18 15:48:23 +0800

    Google: Add Optimization [1055. Shortest Way to Form String]

--- Company-Google/1055. Shortest Way to Form String/main.cpp
+++ Company-Google/1055. Shortest Way to Form String/main.cpp
@@ -25,6 +25,8 @@ using namespace std;
 
 /// Solution:
 //
+// Solution 1: Greedy
+/*
 class Solution {
 public:
     int shortestWay(string source, string target) {
@@ -43,8 +45,39 @@ public:
         return (p + n - 1) / n;
     }
 };
+ */
+
+// Solution 2: Optimized by binary search
+class Solution {
+public:
+    int shortestWay(string source, string target) {
+        vector<vector<int>> poss = vector<vector<int>> (26);
+        for (int i = 0; i < source.size(); ++i)
+            poss[source[i]-'a'].push_back(i);
+
+        int res = 1;
+        for (int i = 0, j = -1; i < target.size();) {
+            auto& pos = poss[target[i]-'a'];
+            if (pos.empty())
+                return -1;
+            auto it = upper_bound(pos.begin(), pos.end(), j);
+            if (it == pos.end()) {
+                ++res;
+                j = -1;
+            } else {
+                j = *it;
+                ++i;
+            }
+        }
+        return res;
+    }
+};
 
 int main() {
-
+    Solution sol = Solution();
+    string s = "abc";
+    string t = "abcbc";
+    int res = sol.shortestWay(s, t);
+    cout << res << endl;
     return 0;
 }
