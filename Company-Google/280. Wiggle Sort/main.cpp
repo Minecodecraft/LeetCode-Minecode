761fcfe8790b0bbe320244bbd374c3b81556dd19
Minecoder <minecoder@163.com>
2020-11-21 23:16:27 +0800

    Google: Add Optimization [280. Wiggle Sort]

--- Company-Google/280. Wiggle Sort/main.cpp
+++ Company-Google/280. Wiggle Sort/main.cpp
@@ -25,6 +25,8 @@ using namespace std;
 
 /// Solution:
 //
+// Solution 1: Brute Force
+/*
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
@@ -32,6 +34,19 @@ public:
         int cnt = nums.size() / 2;
         for (int i = 1, j = nums.size() - cnt; i < j; i += 2, j++) {
             swap(nums[i], nums[j]);
+        }
+    }
+};
+ */
+
+// Solution 2
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        if (nums.empty()) return;
+        for (int i = 0; i < nums.size()-1; ++i) {
+            if (!(i & 1) == (nums[i] > nums[i+1]))
+                swap(nums[i], nums[i+1]);
         }
     }
 };
