8755aa399484518b2d76c7dc67fd326598577086
Minecoder <minecoder@163.com>
2020-12-14 18:29:24 +0800

    Google: Add Solution [524. Longest Word in Dictionary through Deleting]

---
+++ Company-Google/524. Longest Word in Dictionary through Deleting/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  524. Longest Word in Dictionary through Deleting
+//
+//  Created by Jaylen Bian on 12/14/20.
+//  Copyright © 2020 边俊林. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
