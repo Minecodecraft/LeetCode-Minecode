70ab0970170410e641770d7a0efcc58f7900ebe6
Minecoder <minecoder@163.com>
2020-12-14 21:18:31 +0800

    Google: Add Solution [284. Peeking Iterator]

---
+++ Company-Google/284. Peeking Iterator/main.cpp
@@ -0,0 +1,74 @@
+//
+//  main.cpp
+//  284. Peeking Iterator
+//
+//  Created by Jaylen Bian on 12/14/20.
+//  Copyright © 2020 边俊林. All rights reserved.
+//
+
+#include <map>
+#include <set>
+#include <queue>
+#include <string>
+#include <stack>
+#include <vector>
+#include <cstdio>
+#include <numeric>
+#include <cstdlib>
+#include <utility>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+/// Solution:
+class Iterator {
+    struct Data;
+    Data* data;
+    Iterator(const vector<int>& nums);
+    Iterator(const Iterator& iter);
+
+    // Returns the next element in the iteration.
+    int next();
+
+    // Returns true if the iteration has more elements.
+    bool hasNext() const;
+};
+
+// Solution 1:
+class PeekingIterator : public Iterator {
+    int current;
+    bool exist;
+public:
+    PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+        exist = Iterator::hasNext();
+        if (exist)
+            current = Iterator::next();
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    int peek() {
+        return current;
+    }
+
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    int next() {
+        int tmp = current;
+        exist = Iterator::hasNext();
+        if (exist)
+            current = Iterator::next();
+        return tmp;
+    }
+
+    bool hasNext() const {
+        return exist;
+    }
+};
+
+int main() {
+
+    return 0;
+}
--- Company-Google/524. Longest Word in Dictionary through Deleting/main.cpp
+++ Company-Google/524. Longest Word in Dictionary through Deleting/main.cpp
@@ -6,10 +6,52 @@
 //  Copyright © 2020 边俊林. All rights reserved.
 //
 
+#include <map>
+#include <set>
+#include <queue>
+#include <string>
+#include <stack>
+#include <vector>
+#include <cstdio>
+#include <numeric>
+#include <cstdlib>
+#include <utility>
 #include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+/// Solution:
+//
+class Solution {
+public:
+    string findLongestWord(string s, vector<string>& d) {
+        string res = "";
+        vector<bool> vis (26, false);
+        for (char ch: s)
+            vis[ch - 'a'] = true;
+
+        for (int i = 0; i < d.size(); ++i) {
+            int p = 0, j = 0;
+            if (res.length() > d[i].length())
+                continue;
+            for (j = 0; j < d[i].length(); ++j) {
+                if (!vis[d[i][j]-'a']) break;
+                while (p < s.length() && s[p] != d[i][j])
+                    ++p;
+                if (p++ >= s.length())
+                    break;
+            }
+            if (j == d[i].length())
+                res = res.length() == d[i].length() ? min(res, d[i]) : d[i];
+        }
+        return res;
+    }
+};
+
+int main() {
+
     return 0;
 }
