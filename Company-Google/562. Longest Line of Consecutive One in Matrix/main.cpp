f4a96c14bdff5c7e3e710bb31e91cb8d49631425
Minecoder <minecoder@163.com>
2021-01-03 22:22:45 +0800

    Google: Add Optimization [562. Longest Line of Consecutive One in Matrix]

--- Company-Google/562. Longest Line of Consecutive One in Matrix/main.cpp
+++ Company-Google/562. Longest Line of Consecutive One in Matrix/main.cpp
@@ -27,6 +27,7 @@ using namespace std;
 //
 // Solution 1: 4-dimension array
 // O(mn) time complexity, O(mn) space complexity
+/*
 class Solution {
 public:
     int longestLine(vector<vector<int>>& M) {
@@ -49,8 +50,41 @@ public:
         return res;
     }
 };
+ */
+
+// Solution 2: Use 2-dimension array to optimize space complexity.
+class Solution {
+public:
+    int longestLine(vector<vector<int>>& M) {
+        int m = M.size(), n = m == 0 ? 0 : M[0].size();
+        vector<vector<int>> dp (n, vector<int>(4, 0));
+
+        int res = 0;
+        for (int j = 0; j < m; ++j) {
+            vector<vector<int>> tmp (n, vector<int>(4, 0));
+            for (int i = 0; i < n; ++i) {
+                if (M[j][i]) {
+                    tmp[i][0] = i == 0 ? 1 : tmp[i-1][0] + 1;
+                    tmp[i][1] = j == 0 ? 1 : dp[i][1] + 1;
+                    tmp[i][2] = (i == 0 || j == 0) ? 1 : dp[i-1][2] + 1;
+                    tmp[i][3] = (i == n - 1 || j == 0) ? 1 : dp[i+1][3] + 1;
+
+                    res = max(res, max(tmp[i][0], max(tmp[i][1], max(tmp[i][2], tmp[i][3]))));
+                }
+
+            }
+            swap(tmp, dp);
+        }
+        return res;
+    }
+};
 
 int main() {
-
+    Solution sol = Solution();
+    vector<vector<int>> arr ={
+        {0,1,1,0},{0,1,1,0},{0,0,0,1}
+    };
+    int res = sol.longestLine(arr);
+    cout << res << endl;
     return 0;
 }
