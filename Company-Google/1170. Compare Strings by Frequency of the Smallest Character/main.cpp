81eaec19fa9b8469429b87f50801153a62210169
Minecoder <minecoder@163.com>
2021-02-13 18:59:44 +0800

    Google: Add Solution 2 [1170. Compare Strings by Frequency of the Smallest Character]

--- Company-Google/1170. Compare Strings by Frequency of the Smallest Character/main.cpp
+++ Company-Google/1170. Compare Strings by Frequency of the Smallest Character/main.cpp
@@ -25,6 +25,8 @@ using namespace std;
 
 /// Solution:
 //
+// Solution 1: Use binary search
+/*
 class Solution {
     int helper(string& str) {
         vector<int> cnt (26, 0);
@@ -46,6 +48,36 @@ public:
         for (int i = 0; i < m; ++i) {
             int f = helper(qs[i]);
             res[i] = vals.end() - upper_bound(vals.begin(), vals.end(), f);
+        }
+        return res;
+    }
+};
+ */
+
+// Solution 2
+class Solution {
+    int calF(string s) {
+        vector<int> cnt (26, 0);
+        for (char ch: s)
+            cnt[ch-'a']++;
+        for (int ele: cnt)
+            if (ele) return ele;
+        return 0;
+    }
+public:
+    vector<int> numSmallerByFrequency(vector<string>& qs, vector<string>& ws) {
+        vector<int> f (12, 0);
+        for (int i = 0; i < ws.size(); ++i) {
+            int curf = calF(ws[i]);
+            f[curf]++;
+        }
+        for (int i = 9; i >= 0; --i)
+            f[i] += f[i+1];
+
+        vector<int> res;
+        for (int i = 0; i < qs.size(); ++i) {
+            int curf = calF(qs[i]);
+            res.push_back(f[curf+1]);
         }
         return res;
     }
